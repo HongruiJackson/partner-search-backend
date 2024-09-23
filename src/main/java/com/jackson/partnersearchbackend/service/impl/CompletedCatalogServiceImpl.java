fe34e42778cecd2b6e8e6d786599aead8172c00d
JacksonZHR <1024060689@qq.com>
2024-09-23 09:02:34 +0800

    feat: 增加缓存过期时间为1h

--- src/main/java/com/jackson/partnersearchbackend/service/impl/CompletedCatalogServiceImpl.java
+++ src/main/java/com/jackson/partnersearchbackend/service/impl/CompletedCatalogServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static com.jackson.partnersearchbackend.constant.GlobalConstant.GLOBAL_REDIS_KEY;
 import static com.jackson.partnersearchbackend.constant.TagConstant.TAG_LIST_REDIS_KEY;
@@ -38,7 +39,7 @@ public class CompletedCatalogServiceImpl implements CompletedCatalogService {
         if (stringList==null) {
             List<CompletedCatalogVo> completedCatalogList = completedCatalogManager.getCompletedCatalogList();
             try {
-                stringRedisTemplate.opsForValue().set(redisKey,mapper.writeValueAsString(completedCatalogList));
+                stringRedisTemplate.opsForValue().set(redisKey,mapper.writeValueAsString(completedCatalogList),24, TimeUnit.HOURS);
             } catch (JsonProcessingException e) {
                 log.error("缓存插入失败，可能是JSON序列化的问题");
 //                throw new RuntimeException(e);

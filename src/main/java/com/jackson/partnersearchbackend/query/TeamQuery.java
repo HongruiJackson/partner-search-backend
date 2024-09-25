82cd16467543e19cebef27189b4928e554946484
JacksonZHR <1024060689@qq.com>
2024-09-25 09:25:34 +0800

    feat: 队伍查询封装类

---
+++ src/main/java/com/jackson/partnersearchbackend/query/TeamQuery.java
@@ -0,0 +1,48 @@
+package com.jackson.partnersearchbackend.query;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 查询封装类
+ */
+@Data
+public class TeamQuery {
+    /**
+     * 主键
+     */
+    private Long id;
+
+    /**
+     * 队伍名称
+     */
+    private String name;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 最大人数
+     */
+    private Integer maxNum;
+
+    /**
+     * 过期时间
+     */
+    private Date expireTime;
+
+    /**
+     * 创建人id
+     */
+    private Long userId;
+
+    /**
+     * 队伍状态：0公开；1私密；2加密，默认0
+     */
+    private Integer teamStatus;
+}
